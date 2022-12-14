2dc7316e31a131d983e0980f8527dc4120a3b358
PacioTenPros <paciobumbum@gmail.com>
2022-12-14 17:14:41 +0100

    Next.js v13 "/app" directory implementation

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Blog of a 15yo developer</title>
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <link rel='icon' href='/favicon.ico' />
+    </>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the blog</h1>
+      <p>Here you will see the changes</p>
+    </div>
+  );
+};
+
+export default HomePage;
