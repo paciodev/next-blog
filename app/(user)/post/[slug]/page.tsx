a0f62b03b87927015f7587561a541f65315df1f5
PacioTenPros <paciobumbum@gmail.com>
2022-12-15 20:25:42 +0100

    Sanity Iframe + basic single post page

---
+++ app/(user)/post/[slug]/page.tsx
@@ -0,0 +1,26 @@
+import { groq } from 'next-sanity';
+import { client } from '../../../../lib/sanity.client';
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+const Post = async ({ params: { slug } }: Props) => {
+  const query = groq`
+    *[_type=='post' && slug.current == $slug] [0]
+    {
+      ...,
+      author->,
+      categories[]->
+    }
+  `;
+
+  const post: Post = await client.fetch(query, { slug });
+
+  console.log(post);
+  return <div>Post: {slug}</div>;
+};
+
+export default Post;
