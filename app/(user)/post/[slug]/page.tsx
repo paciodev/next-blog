1b1dad6fc66d45813a8e2efb61c18f0d3a09cb04
PacioTenPros <paciobumbum@gmail.com>
2022-12-15 21:00:36 +0100

    Advanced Blog Page with @portabletext/react

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -1,5 +1,9 @@
 import { groq } from 'next-sanity';
+import Image from 'next/image';
 import { client } from '../../../../lib/sanity.client';
+import urlFor from '../../../../lib/urlFor';
+import { PortableText } from '@portabletext/react';
+import { RichTextComponents } from '../../../../components/RichTextComponents';
 
 type Props = {
   params: {
@@ -19,8 +23,67 @@ const Post = async ({ params: { slug } }: Props) => {
 
   const post: Post = await client.fetch(query, { slug });
 
-  console.log(post);
-  return <div>Post: {slug}</div>;
+  return (
+    <article className='px-10 pb-28'>
+      <section className='space-y-2 border border-accent text-white'>
+        <div className='relative min-h-56 flex flex-col md:flex-row justify-between'>
+          <div className='absolute top-0 w-full h-full opacity-10 blur-sm p-10'>
+            <Image
+              className='object-cover object-center mx-auto'
+              src={urlFor(post.mainImage).url()}
+              alt={post.author.name}
+              fill
+            />
+          </div>
+
+          <section className='p-5 bg-accent w-full'>
+            <div className='flex flex-col md:flex-row justify-between gap-y-5'>
+              <div>
+                <h1 className='text-4xl font-extrabold'>{post.title}</h1>
+                <p className=''>
+                  {new Date(post._createdAt).toLocaleDateString('pl-PL', {
+                    day: 'numeric',
+                    month: 'long',
+                    year: 'numeric',
+                  })}
+                </p>
+              </div>
+              <div className='flex items-center space-x-2'>
+                <Image
+                  className='rounded-full'
+                  src={urlFor(post.author.image).url()}
+                  alt={post.author.name}
+                  height={40}
+                  width={40}
+                />
+
+                <div className='w-64'>
+                  <h3 className='text-lg font-bold'>{post.author.name}</h3>
+                  <div>{/* AUTHOR BIO */}</div>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <h2 className='italic pt-10'>{post.description}</h2>
+              <div className='flex items-center justify-end mt-auto space-x-2'>
+                {post.categories.map((c) => (
+                  <p
+                    key={c._id}
+                    className='bg-gray-800 text-white px-3 py-1 rounded-full text-sm font-semibold mt-4'
+                  >
+                    {c.title}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </section>
+        </div>
+      </section>
+
+      <PortableText value={post.body} components={RichTextComponents} />
+    </article>
+  );
 };
 
 export default Post;
